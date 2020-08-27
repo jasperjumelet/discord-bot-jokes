e5607aabd8da87b0cf071b45b73c23fd6271c8e6
jasper jumelet <45312097+jasperjumelet@users.noreply.github.com>
2020-08-27 11:10:07 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -2,21 +2,33 @@
 import os
 import discord
 from dotenv import load_dotenv
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("jokes.csv")
+joke = df.sample(axis= 0)
 
 class MyClient(discord.Client):
-    
+
     async def on_ready(self):
         print(f'{self.user} has connected to Discord!')
 
     async def on_message(self, message):
-        content = message.content
-        msg = "Your typed message is: " + content[3:]
-        if message.content.startswith("?dj"):
-            await message.channel.send(msg)
+        global joke
+        #content = message.content
+        
+        if message.content.startswith("?joke"):
+            question = np.array(joke["Question"])
+            await message.channel.send(question[0])
+        
+        if message.content.startswith("?answer"):
+            answer = np.array(joke["Answer"])
+            await message.channel.send(answer[0])
+            joke=df.sample(axis=0)
 
-    async def on_typing(self, channel, user, when):
-        await self.wait_until_ready()
-        await channel.send(f"Hey joh dikzak {str(user)[:-5]} wat zit je te typen??")
+    # async def on_typing(self, channel, user, when):
+    #     await self.wait_until_ready()
+    #     await channel.send(f"Hey joh dikzak {str(user)[:-5]} wat zit je te typen??")
 
 def main():
     load_dotenv()
